10ad2a43f09a06677d4f0d629eeb226dfca5d4a2
h0plyn <ricky@rickyrhodes.com>
2020-09-03 11:27:03 -0400

    refactor codewars: Parity Outlier

--- src/codewars/6-kyu-parity-outlier.js
+++ src/codewars/6-kyu-parity-outlier.js
@@ -1,8 +1,6 @@
 function findOutlier(integers){
-    let evenArr = [];
-    let oddArr = [];
-
-    integers.forEach(num => {num % 2 === 0 ? evenArr.push(num) : oddArr.push(num)})
-
+    let evenArr = integers.filter(num => num % 2 === 0);
+    let oddArr = integers.filter(num => num % 2 === 1);
+   
     return oddArr.length < evenArr.length ? oddArr[0] : evenArr[0];
 }
