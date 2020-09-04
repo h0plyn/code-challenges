b0d94cecf0478f85d1d37ba77430266318851d57
h0plyn <ricky@rickyrhodes.com>
2020-09-04 19:06:41 -0400

    refactor codewars: Duplicate Encoder with .map

--- src/codewars/6-kyu-duplicate-encoder.js
+++ src/codewars/6-kyu-duplicate-encoder.js
@@ -1,23 +1,11 @@
-function duplicateEncode(word){
-    let charArr = word.toLowerCase().split('');
-    let encodedString = '';
-    let cache = {};
-    
-    charArr.forEach(el => {
-        cache[el] = (cache[el] || 0) + 1;
-    })
-
-    for (let i = 0; i < charArr.length; i++) {
-        let currentChar = charArr[i];
-
-        if (cache[currentChar] > 1) {
-            encodedString += ')';
-        } else {
-            encodedString += '(';
-        }
-    }
-
-    return encodedString;
+function duplicateEncode(word) {
+    return word
+        .toLowerCase()
+        .split('')
+        .map((element, index, word) => {
+            return word.indexOf(element) == word.lastIndexOf(element) ? '(' : ')'
+        })
+        .join('')
 }
 
-console.log(duplicateEncode('Success'))
+console.log(duplicateEncode("Success"))
