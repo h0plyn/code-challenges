91fd726166b70b90882d5a8e2448133e1ccac99f
h0plyn <ricky@rickyrhodes.com>
2020-09-06 18:55:50 -0400

    add hackerrank: Sock Merchant

---
+++ src/hackerrank/sock-merchant.js
@@ -0,0 +1,16 @@
+function sockMerchant(n, ar) {
+    let sortedSocks = ar.sort((a, b) => a - b);
+    
+    let pairCount = 0;
+    for (let i = 0; i<n; i++) {
+        let currentSock = sortedSocks[i];
+        let nextSock = sortedSocks[i+1];
+
+        if (currentSock == nextSock) {
+            pairCount++
+            i++
+        }
+    }
+
+    return pairCount
+}
