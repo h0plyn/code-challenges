63588c97235cfca27ff9e340846c2b4b353fa366
Ricky Rhodes <ricky@rickyrhodes.com>
2020-12-19 19:05:55 -0500

    add Bubble Sort

---
+++ src/algoexpert/bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+
+  return arr;
+}
