8d03a4524651ed286eda66b2e329874a15cf1b3c
Ricky Rhodes <ricky@rickyrhodes.com>
2021-03-03 20:26:15 -0500

    add Sorted Squares optimized

--- algoexpert/sortedSquares.js
+++ algoexpert/sortedSquares.js
@@ -1,3 +1,28 @@
+// O(n)t | O(n)s
+function sortedSquaredArray(array) {
+  let sortedSquares = new Array(array.length).fill(0)
+	let leftIdx = 0
+	let rightIdx = array.length - 1
+
+	for (let i = array.length - 1; i >= 0; i--) {
+		let smallerNum = array[leftIdx]
+		let largerNum = array[rightIdx]
+
+
+		if (Math.abs(smallerNum) > Math.abs(largerNum)) {
+			sortedSquares[i] = smallerNum * smallerNum
+			leftIdx++
+		} else {
+			sortedSquares[i] = largerNum * largerNum
+			rightIdx--
+		}
+	}
+  return sortedSquares
+}
+
+// Brute Force
+// O(nlogn)t | O(n)s
+// Built in sort is nlogn
 function sortedSquaredArray(array) {
   let squares = [];
 
