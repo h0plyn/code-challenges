1531898a198f05c93bf32eb39c3c37a8b8b4e9b9
Ricky Rhodes <ricky@rickyrhodes.com>
2021-03-11 09:27:51 -0500

    add notes to N-ary tree post order

--- leetcode/naryPostorderBFS.js
+++ leetcode/naryPostorderBFS.js
@@ -1,14 +1,14 @@
 // O(n)t | O(n)s - where n is the number of nodes in the tree
 var postorder = function (root) {
   if (!root) return [];
-  const stack = [root];
+  const stack = [root]; // initialize stack with the root
   const result = [];
 
   while (stack.length) {
-    let currentNode = stack.pop();
-    stack.push(...currentNode.children);
-    result.push(currentNode.val);
+    let currentNode = stack.pop(); // make top of stack the current node to look at
+    stack.push(...currentNode.children); // push all children to stack so they can be explored
+    result.push(currentNode.val); // add the current value to the stack noted that we have seen this node
   }
 
-  return result.reverse();
+  return result.reverse(); // return the reversed order
 };
