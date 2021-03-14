4d0812bdee4e11be41f23c97bc112a9e2d521bac
Ricky Rhodes <ricky@rickyrhodes.com>
2021-03-13 22:19:13 -0500

    fix space complexity

--- educative/maxSumSubarraySize.js
+++ educative/maxSumSubarraySize.js
@@ -1,13 +1,15 @@
-// O(n)t | O(n)s
+// O(n)t | O(1)s
 // Sliding window pattern
 function maxSumSubarrayOfK(arr, k) {
+  debugger;
   let maxSum = 0;
   let windowStart = 0;
   let windowSum = 0;
 
   for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
     windowSum += arr[windowEnd]; // add to current window sum
-    if (windowEnd >= k - 1) { // if at end of current window constraint
+    if (windowEnd >= k - 1) {
+      // if at end of current window constraint
       maxSum = Math.max(windowSum, maxSum); // check if current window holds greatest sum
       windowSum -= arr[windowStart]; // subtract the tail element
       windowStart += 1; // slide window forward
