388365cba078142ecfda52a93ff4c48c3a3f705f
Ricky Rhodes <ricky@rickyrhodes.com>
2021-05-10 23:40:46 -0400

    update with sliding window

--- leetcode/findMaxConsecutiveOnes.ts
+++ leetcode/findMaxConsecutiveOnes.ts
@@ -14,3 +14,29 @@ function findMaxConsecutiveOnes(nums: number[]): number {
   }
   return maxOnes;
 }
+
+// Sliding Window
+// O(n)t | O(1)s
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let left: number = 0;
+  let right: number = 0;
+  let maxOnes: number = 0;
+  let zeros: number = 0;
+
+  while (right < nums.length) { // while right is inbounds
+      if (nums[right] === 0) {
+          zeros++
+      }
+
+      while (zeros === 2) {
+          if (nums[left] === 0) {
+              zeros-- // remove the zero from the window
+          }
+          left++ // increment left side of window until only 1 zero is in the window
+      }
+
+      maxOnes = Math.max(maxOnes, right - left + 1) // update longest sequence
+      right++ // expand window
+  }
+  return maxOnes;
+};
