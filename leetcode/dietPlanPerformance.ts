00816e19e062bd763f74af07c02be5914d724e5b
Ricky Rhodes <ricky@rickyrhodes.com>
2021-05-16 20:58:46 -0400

    add optimized approach

--- leetcode/dietPlanPerformance.ts
+++ leetcode/dietPlanPerformance.ts
@@ -17,3 +17,30 @@ function dietPlanPerformance(
   }
   return points;
 }
+
+// Sliding Window
+// O(n)t | O(1)s
+function dietPlanPerformance(
+  calories: number[],
+  k: number,
+  lower: number,
+  upper: number
+): number {
+  let points: number = 0;
+  let start: number = 0;
+  let calCount: number = 0;
+
+  for (let end = 0; end < calories.length; end++) {
+    calCount += calories[end]; // add current element to calCount
+
+    if (end < k - 1) continue; // if the window hasn't reach k length, keep adding elements
+    if (end > k - 1) { // if the window is larger than the fixed window size (k)
+      calCount -= calories[start]; // remove the starting element value from calCount
+      start++; // increment the start to
+    }
+
+    if (calCount > upper) points++; // add points if over goal
+    if (calCount < lower) points--; // subtract points if under goal
+  }
+  return points;
+}
--- leetcode/removeLinkedListElements.ts
+++ leetcode/removeLinkedListElements.ts
@@ -1,27 +1,26 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
 // O(n)t | O(1)s
 function removeElements(head: ListNode | null, val: number): ListNode | null {
   if (!head) return null;
 
   while (head) {
-    if (head.val === val) head = head.next; // if our head needs to be removed, move our head forward
+    if (head.val === val) head = head.next;
+    // if our head needs to be removed, move our head forward
     else break;
   }
 
   let curr: ListNode | null = head;
   while (curr && curr.next) {
-    if (curr.next.val === val) { // if our next node needs to be removed
+    if (curr.next.val === val) {
+      // if our next node needs to be removed
       curr.next = curr.next.next; // set our curr.next to skip the node to remove
     } else {
       curr = curr.next; // move pointer forward
