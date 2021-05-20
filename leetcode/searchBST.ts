ba9f1be3afe9b6315ab64a86b065d6b5e13a9d53
Ricky Rhodes <ricky@rickyrhodes.com>
2021-05-19 22:19:34 -0400

    add BST Search Iterative

--- leetcode/searchBST.ts
+++ leetcode/searchBST.ts
@@ -1,21 +1,31 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
-function searchBST(root: TreeNode | null, val: number): TreeNode | null {
-  if (root === null) return root;
+type Result = TreeNode | null;
+
+// O(logn)t | O(n)s
+function searchBST(root: Result, val: number): Result {
+  if (root === null) return null;
   if (root.val === val) return root;
 
   if (val < root.val) return searchBST(root.left, val);
   if (val > root.val) return searchBST(root.right, val);
+  return null;
+}
+
+function searchBSTIterative(root: Result, val: number): Result {
+  while (root) {
+    if (root.val === val) return root;
+    else if (val < root.val) root = root.left;
+    else if (val > root.val) root = root.right;
+  }
+  return null;
 }
