23c48fd69907d62d2f0385f2af0f4c88347ec293
Ricky Rhodes <ricky@rickyrhodes.com>
2021-06-02 08:39:51 -0400

    add another First and Last implementation

--- leetcode/firstAndLastIdx.js
+++ leetcode/firstAndLastIdx.js
@@ -4,6 +4,7 @@
  * @return {number[]}
  */
 
+// O(logn)t | O(1)s - where n is the length of the input array
 var searchRange = function (nums, target) {
   let left = 0,
     right = nums.length - 1,
@@ -28,6 +29,45 @@ var searchRange = function (nums, target) {
 
   while (nums[first - 1] === target) first -= 1;
   while (nums[last + 1] === target) last += 1;
+
+  return [first, last];
+};
+
+// O(logn)t | O(1)s - where n  is the length of the input array
+var searchRange = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1,
+    first = -1,
+    last = -1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      first = mid;
+      right = mid - 1;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
+    }
+  }
+
+  left = 0;
+  right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      last = mid;
+      left = mid + 1;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
+    }
+  }
 
   return [first, last];
 };
